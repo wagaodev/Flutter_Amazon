6a77662b4e2e979a6c4138ab95e0d1e9e4c78305
Wagner Goulart <wcfx.dev@gmail.com>
2021-11-14 03:24:50 -0300

    feat: search box with right style

--- lib/screens/Home/Widgets/search_bar.dart
+++ lib/screens/Home/Widgets/search_bar.dart
@@ -6,6 +6,17 @@ Widget buildSearchBar() => Padding(
       child: TextField(
         decoration: InputDecoration(
           hintText: 'Search a book',
+          fillColor: Colors.black.withOpacity(0.1),
+          filled: true,
+          prefixIcon: Icon(Icons.search),
+          contentPadding: EdgeInsets.symmetric(
+            horizontal: Constants.kPadding,
+            vertical: 0,
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(2 * Constants.kPadding),
+            borderSide: BorderSide.none,
+          ),
         ),
       ),
     );
