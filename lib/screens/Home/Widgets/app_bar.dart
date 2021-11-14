4d241e6fa2ad513384f26ba6b73a46dee7888693
Wagner Goulart <wcfx.dev@gmail.com>
2021-11-14 02:40:28 -0300

    feat: create constants to help project

--- lib/screens/Home/Widgets/app_bar.dart
+++ lib/screens/Home/Widgets/app_bar.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_amazon/utils/constants.dart';
 
 AppBar buildAppBar() => AppBar(
       backgroundColor: Colors.transparent,
       elevation: 0,
       centerTitle: false,
       title: Image.asset("assets/images/amazon.png", height: 25),
+      actions: [
+        Padding(
+          padding: EdgeInsets.all(Constants.kPadding),
+          child: CircleAvatar(
+            backgroundImage: AssetImage("assets/images/profile.jpeg"),
+          ),
+        ),
+      ],
     );
