3b2bfabd8b4bdcd7a3a69162e68edeaf190c8de8
Wagner Goulart <wcfx.dev@gmail.com>
2021-11-16 02:32:20 -0300

    feat: create layout action bottom

--- lib/screens/Home/Widgets/fab.dart
+++ lib/screens/Home/Widgets/fab.dart
@@ -1,10 +1,128 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_amazon/utils/constants.dart';
 
 class BuildFAB extends StatelessWidget {
   const BuildFAB({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(onPressed: () {});
+    return FloatingActionButton(
+      onPressed: () {
+        showModalBottomSheet(
+          context: context,
+          builder: (context) => Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 40,
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 40),
+                  Text(
+                    'Shop By',
+                    style: TextStyle(
+                      color: Colors.black54,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Category',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                        ),
+                      ),
+                      Icon(
+                        Icons.search,
+                        color: Colors.deepOrange,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: Constants.kPadding),
+                  _buildCategory(
+                    category: "Books",
+                    iconData: Icons.book,
+                    color: Colors.green,
+                  ),
+                  _buildCategory(
+                    category: "Video Games",
+                    iconData: Icons.sports_esports,
+                    color: Colors.deepOrange,
+                  ),
+                  _buildCategory(
+                    category: "Music & Movies",
+                    iconData: Icons.movie,
+                    color: Colors.blueAccent,
+                  ),
+                  _buildCategory(
+                    category: "Grocery",
+                    iconData: Icons.local_grocery_store,
+                    color: Colors.cyan,
+                  ),
+                  _buildCategory(
+                    category: "Health & Beauty",
+                    iconData: Icons.healing,
+                    color: Colors.pink,
+                  ),
+                  _buildCategory(
+                    category: "Sports",
+                    iconData: Icons.sports_basketball_rounded,
+                    color: Colors.red,
+                  ),
+                  SizedBox(height: 5 * Constants.kPadding),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+      backgroundColor: Colors.white,
+      child: Icon(
+        Icons.list_alt_rounded,
+        color: Colors.blue,
+      ),
+    );
+  }
+
+  Widget _buildCategory({
+    required String category,
+    required IconData iconData,
+    required Color color,
+  }) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: Constants.kPadding,
+      ),
+      child: InkWell(
+        onTap: () {},
+        borderRadius: BorderRadius.circular(25),
+        child: Container(
+          width: double.infinity,
+          child: Wrap(
+            crossAxisAlignment: WrapCrossAlignment.center,
+            spacing: 2 * Constants.kPadding,
+            children: [
+              CircleAvatar(
+                backgroundColor: color,
+                child: Icon(
+                  iconData,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                category,
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/Home/Widgets/new_book.dart
+++ lib/screens/Home/Widgets/new_book.dart
@@ -9,99 +9,105 @@ class BuildNewBook extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: 2 * Constants.kPadding,
-          vertical: Constants.kPadding,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              'Hot New Releases',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Icon(Icons.keyboard_arrow_right),
-          ],
-        ),
-      ),
-      InkWell(
-        onTap: () {},
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: Constants.kPadding,
-              ),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                elevation: 4,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Image.asset(
-                    newBook.image,
-                    height: 180,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  ),
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 2 * Constants.kPadding,
+            vertical: Constants.kPadding,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Hot New Releases',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 2 * Constants.kPadding),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        newBook.title,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        newBook.subtitle,
-                        style: TextStyle(
-                          fontSize: 10,
-                          color: Colors.blueGrey,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        textAlign: TextAlign.left,
-                      ),
-                    ],
+              Icon(Icons.keyboard_arrow_right),
+            ],
+          ),
+        ),
+        InkWell(
+          onTap: () {},
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: Constants.kPadding,
+                ),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text("4,8"),
-                    style: TextButton.styleFrom(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: Constants.kPadding,
-                        vertical: Constants.kPadding,
-                      ),
-                      backgroundColor: Colors.deepOrange,
-                      primary: Colors.white,
-                      minimumSize: Size(5, 5),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
+                  elevation: 4,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                      newBook.image,
+                      height: 180,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
                     ),
-                  )
-                ],
+                  ),
+                ),
               ),
-            ),
-          ],
+              Padding(
+                padding:
+                    EdgeInsets.symmetric(horizontal: 2 * Constants.kPadding),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          newBook.title,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          newBook.subtitle,
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: Colors.blueGrey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                      ],
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text("4,8"),
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: Constants.kPadding,
+                          vertical: 2,
+                        ),
+                        backgroundColor: Colors.deepOrange,
+                        primary: Colors.white,
+                        minimumSize: Size(5, 5),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-    ]);
+        SizedBox(
+          height: MediaQuery.of(context).padding.bottom,
+        )
+      ],
+    );
   }
 }
